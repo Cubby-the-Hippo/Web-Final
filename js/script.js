d96b4828b6fe68ac83f424d54a1f7ca635216436
Cubby-the-Hippo <alex87funny@gmail.com>
2024-11-07 16:32:35 -0800

    3rd commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 function loadHours() {
     document.getElementById('opening-hours').innerHTML = `
         Monday - Thursday: 10 AM - 5 PM<br>
-        Friday: 10 AM - 4 PM<br>
-        <br>
+        Friday: 10 AM - 4 PM<br><br>
         For new purchases and longer consultations, please make an appointment with our master. A short call or email is enough!
     `;
 }
